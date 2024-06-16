e3f9dc0a433aebaa36703ef97c5ca4062ffe1549
Nitish <nathnitish157@gmail.com>
2024-06-16 16:43:41 +0530

    Resend Email Verification Token (OTP)

--- controllers/user.js
+++ controllers/user.js
@@ -17,6 +17,8 @@ exports.create = async (req, res) => {
     const randomVal = Math.round(Math.random() * 9);
     OTP += randomVal;
   }
+
+  //storing OTP inside DataBase
   const newEmailVerificationToken = new EmailVerificationToken({
     owner: newUser._id,
     token: OTP,
@@ -52,20 +54,102 @@ exports.create = async (req, res) => {
 
 exports.verifyEmail = async (req, res) => {
   const { userId, OTP } = req.body;
+
+  // Check if userId is valid
   if (!isValidObjectId(userId)) return res.json({ error: "Invalid User" });
+
+  // Find the user by ID
   const user = await User.findById(userId);
   if (!user) return res.json({ error: "User Not Found!" });
-  if (user.isVerified) res.json({ error: "User is already verified" });
 
+  // Check if the user is already verified
+  if (user.isVerified) return res.json({ error: "User is already verified" });
+
+  // Find the verification token
   const token = await EmailVerificationToken.findOne({ owner: userId });
   if (!token) return res.json({ error: "Token not found!" });
 
+  // Compare the provided OTP with the stored token
   const isMatched = await token.compareToken(OTP);
   if (!isMatched) return res.json({ error: "Please submit a valid OTP!" });
 
+  // Mark the user as verified
   user.isVerified = true;
   await user.save();
   await EmailVerificationToken.findByIdAndDelete(token._id);
 
+  // Create a transport object for sending email
+  var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "04a8826f0c60a6",
+      pass: "a5de2911505e5c",
+    },
+  });
+
+  // Send the welcome email
+  transport.sendMail({
+    from: "verification@reviewpp.com",
+    to: user.email,
+    subject: "Welcome Email",
+    html: `<h1>Welcome to our app and thanks for choosing us.</h1>`,
+  });
+
+  // Send a response indicating successful verification
   res.json({ message: "Your email is verified!" });
+};
+
+exports.resandEmailVerificationToken = async (req, res) => {
+  const { userId } = req.body;
+
+  // Find the user by ID
+  const user = await User.findById(userId);
+  if (!user) return res.json({ error: "User Not Found!" });
+
+  if (user.isVerified) return res.json({ error: "User is already verified" });
+
+  const alreadyHasToken = await EmailVerificationToken.findOne({
+    owner: userId,
+  });
+  if (alreadyHasToken)
+    return res.json({
+      error: "You can request for another token after one hour!",
+    });
+
+  //generate 6 digit OTP
+  let OTP = "";
+  for (let i = 0; i <= 5; i++) {
+    const randomVal = Math.round(Math.random() * 9);
+    OTP += randomVal;
+  }
+
+  const newEmailVerificationToken = new EmailVerificationToken({
+    owner: user._id,
+    token: OTP,
+  });
+  await newEmailVerificationToken.save();
+
+  //Send the OTP to the user
+  var transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "04a8826f0c60a6",
+      pass: "a5de2911505e5c",
+    },
+  });
+
+  transport.sendMail({
+    from: "verification@reviewpp.com",
+    to: user.email,
+    subject: "Email Verification",
+    html: `<p>Your Email Verification Token</p>
+    <h1>${OTP}</h1>`,
+  });
+
+  res.status(201).json({
+    message:
+      "Please verify your email. OTP has been sent to your email account!",
+  });
 };
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { create } = require("../controllers/user");
+const {
+  create,
+  verifyEmail,
+  resandEmailVerificationToken,
+} = require("../controllers/user");
 const { userValidator, validate } = require("../middlewares/validator");
 
 const router = express.Router();
 
 router.post("/create", userValidator, validate, create);
+router.post("/email-verify", verifyEmail);
+router.post("/resend-email-verification-token", resandEmailVerificationToken);
 
 module.exports = router;
